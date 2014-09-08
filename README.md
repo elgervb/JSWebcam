b350fbbaf65de479a608a105c8f4ef321a895d02
Elger van Boxtel <elgervb@gmail.com>
2014-09-08 16:53:32 +0200

    use a fallback for mobile devices
    use an optional fallback to use the camera of mobile devices or to
    upload pictures from disc.

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 JSWebcam
 ========
 
-Make use of the webcam in the broser
+Make use of the webcam in the browser, using an optional fallback to use the camera of mobile devices or to upload pictures from disc.
+
 
 Methods
 -------
@@ -10,12 +11,18 @@ Methods
 | Method      | Description                                               |
 |-------------|-----------------------------------------------------------|
 | element     | Returns the actual video HTMLElement                      |
-| start       | Enables the webcam, will ask the user to share his webcam |
+| fallback    | Fallback mechanism to select files. Use this when you want 
+                to take pics from a mobile device or upload pictures from 
+                disc                                                      |
 | isStarted   | Checks whether the webcam is started                      |
 | isSupported | Checks whether or not the API is supported by the browser |
+| start       | Enables the webcam, will ask the user to share his webcam |
 | stop        | Stops the Webcam object                                   |
 | takePicture | Take a still image of the webcam, returns a Blob          |
 | uriToBlob   | Takes in a dataUri and transforms it into a Blob          |
+| useFallback | Sets if a fallback mechanism should be in place to select 
+                files. Set this to true when you want to take pics from a 
+                mobile device or upload pictures from dics                |
 
 
 Usage
@@ -41,7 +48,12 @@ else {
 }
 ```
 
-Links
+Reading material
 -----
+http://dev.w3.org/2011/webrtc/editor/archives/20140619/getusermedia.html
+http://dev.w3.org/2009/dap/camera/
+
 https://developer.mozilla.org/en-US/docs/NavigatorUserMedia.getUserMedia
 https://developer.mozilla.org/en-US/docs/Web/Guide/API/WebRTC
+
+http://www.html5rocks.com/en/tutorials/getusermedia/intro/
--- examples/impl.js
+++ examples/impl.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	document.getElementById('image').style.display = 'none';
 	document.getElementById('thumb').style.display = 'none';
+	document.getElementById('video').style.display = 'none';
 
 	var webcam = new Webcam('#video');
 
@@ -11,17 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.getElementById('image').style.display = 'none';
 		document.getElementById('thumb').style.display = 'none';
 	}, false);
+
 	document.getElementById('start').addEventListener('click', function(e){
 		e.preventDefault();
+
 		if (webcam.isSupported()) {
 		    webcam.start({video: true, audio: false}, function(stream) {
 		      // success function, video is streaming...
+		      document.getElementById('video').style.display = 'block';
 		    }, function(e) {
 		      alert('Oops, something went wrong');
 		    });
 		}
 		else {
-		    alert('API is not supported by your browser');
+			// if (window.navigator.userAgent.match(/Mobi/)){ // mobile browser...
+				webcam.useFallback(true);
+			// }
+		 //    else{
+		 //    	alert('API is not supported by your browser');
+		 //    }
 		}
 	}, false);
 
@@ -29,24 +38,41 @@ document.addEventListener('DOMContentLoaded', function () {
 		e.preventDefault();
 		if (webcam.isStarted()){
 			// now take an picture using the webcam
-		    var pic = webcam.takePicture();
-
-		    // and assign it to an image
-		    document.querySelector('#image').src = pic;
-		    document.querySelector('#image').style.display = 'inline';
+		    webcam.takePicture(function(pic){
+		    	// and assign it to an image
+			    document.querySelector('#image').src = pic;
+			    document.querySelector('#image').style.display = 'inline';
+		    });
 
 		    // now take an picture using the webcam and make a square centered thumb
-		    var pic = webcam.takePicture(function(ctx, canvas){
+		    webcam.takePicture(function(pic){
+		    	// and assign it to an image
+			    document.querySelector('#thumb').src = pic;
+			    document.querySelector('#thumb').style.display = 'inline';
+		    },function(ctx, canvas){
 		        var min = Math.min(canvas.width,canvas.height);
 		        var max = Math.max(canvas.width,canvas.height);
 		        canvas.width = min;
 		        canvas.height = min;
 		        ctx.drawImage(webcam.element(), (max-min)/2, 0, min,min, 0,0,min,min);
 		      });
+	    }
+	}, false);
 
-		    // and assign it to an image
+	document.getElementById('fallback').addEventListener('click', function(e){
+
+		// now take an picture using the webcam and make a square centered thumb
+	    webcam.fallback(function(pic){
+	    	// and assign it to an image
 		    document.querySelector('#thumb').src = pic;
 		    document.querySelector('#thumb').style.display = 'inline';
-	    }
+	    },function(ctx, canvas){
+	        var min = Math.min(canvas.width,canvas.height);
+	        var max = Math.max(canvas.width,canvas.height);
+	        canvas.width = min;
+	        canvas.height = min;
+	        ctx.drawImage(webcam.element(), (max-min)/2, 0, min,min, 0,0,min,min);
+	    });
+
 	}, false);
 });
